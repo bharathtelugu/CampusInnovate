c733bc8e5a9a8eeb7526bc3f44d8722d35cb44f9
bharathtelugu <bharath738610@gmail.com>
2025-09-21 20:50:53 +0530

    updated

--- campusinnovate/urls.py
+++ campusinnovate/urls.py
@@ -1,24 +1,29 @@
 from django.contrib import admin
 from django.urls import path, include
-from events.views import EventListView # Import the new EventListView
+from django.conf import settings
+from django.conf.urls.static import static
+from events.views import EventListView # Import the homepage view
 
 urlpatterns = [
+    # Admin Panel
     path('admin/', admin.site.urls),
-    
-    # Make the Event List the new homepage
-    path('', EventListView.as_view(), name='home'), 
-    
-    # Account URLs (login, register, etc.)
-    path('accounts/', include('accounts.urls')),
-    
-    # Event Detail URLs
-    path('events/', include('events.urls')),
 
-    # Judging URLs
-    path('judging/', include('submissions.urls')),
-    
-    # ... any other app URLs ...
+    # App URLs
+    # This includes all the URL patterns from each of our apps.
+    path('accounts/', include('accounts.urls')),
+    path('communications/', include('communications.urls')),
+    path('events/', include('events.urls')),
+    path('submissions/', include('submissions.urls')),
+    path('teams/', include('teams.urls')),
+    path('tracking/', include('tracking.urls')),
+
+    # Homepage
+    # The root URL of the site will show the list of all published events.
+    path('', EventListView.as_view(), name='home'),
 ]
-# This is required to serve media files (like submission images) during development
+
+# This is a helper for serving user-uploaded media files (like submission images)
+# during development. It should NOT be used in a production environment.
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
